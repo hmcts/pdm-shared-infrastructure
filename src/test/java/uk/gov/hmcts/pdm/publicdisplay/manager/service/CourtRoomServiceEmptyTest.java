91bc102d1b29a351ae06042a16cb283c008c3e0d
AnasGhafouri1 <Anas.Ghafouri@HMCTS.NET>
2024-01-23 10:53:20 +0000

    added junit tests

---
+++ src/test/java/uk/gov/hmcts/pdm/publicdisplay/manager/service/CourtRoomServiceEmptyTest.java
@@ -0,0 +1,151 @@
+package uk.gov.hmcts.pdm.publicdisplay.manager.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.hmcts.pdm.business.entities.xhbcourt.XhbCourtDao;
+import uk.gov.hmcts.pdm.business.entities.xhbcourt.XhbCourtRepository;
+import uk.gov.hmcts.pdm.business.entities.xhbcourtroom.XhbCourtRoomDao;
+import uk.gov.hmcts.pdm.business.entities.xhbcourtroom.XhbCourtRoomRepository;
+import uk.gov.hmcts.pdm.business.entities.xhbcourtsite.XhbCourtSiteDao;
+import uk.gov.hmcts.pdm.business.entities.xhbcourtsite.XhbCourtSiteRepository;
+import uk.gov.hmcts.pdm.publicdisplay.common.test.AbstractJUnit;
+import uk.gov.hmcts.pdm.publicdisplay.manager.dto.CourtDto;
+import uk.gov.hmcts.pdm.publicdisplay.manager.dto.CourtRoomDto;
+import uk.gov.hmcts.pdm.publicdisplay.manager.dto.XhibitCourtSiteDto;
+import uk.gov.hmcts.pdm.publicdisplay.manager.service.api.ICourtRoomService;
+import uk.gov.hmcts.pdm.publicdisplay.manager.web.courtroom.CourtRoomAmendCommand;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+abstract class CourtRoomServiceEmptyTest extends AbstractJUnit {
+
+    /** The class under test. */
+    protected ICourtRoomService classUnderTest;
+
+    /** The mock Display Location Repo. */
+    protected XhbCourtRepository mockCourtRepo;
+    protected XhbCourtSiteRepository mockCourtSiteRepo;
+    protected XhbCourtRoomRepository mockCourtRoomRepo;
+
+
+    protected static final String NOT_EQUAL = "Not equal";
+    protected static final String FALSE = "False";
+    protected static final String DESCRIPTION = "description";
+
+
+    /**
+     * Setup.
+     */
+    @BeforeEach
+    public void setup() {
+        // Create a new version of the class under test
+        classUnderTest = new CourtRoomService();
+
+        // Setup the mock version of the called classes
+        mockCourtRepo = createMock(XhbCourtRepository.class);
+        mockCourtSiteRepo = createMock(XhbCourtSiteRepository.class);
+        mockCourtRoomRepo = createMock(XhbCourtRoomRepository.class);
+
+        ReflectionTestUtils.setField(classUnderTest, "xhbCourtRepository", mockCourtRepo);
+        ReflectionTestUtils.setField(classUnderTest, "xhbCourtSiteRepository", mockCourtSiteRepo);
+        ReflectionTestUtils.setField(classUnderTest, "xhbCourtRoomRepository", mockCourtRoomRepo);
+
+    }
+
+    @Test
+    void emptyCourtsTest() {
+
+        List<XhbCourtDao> courtDaoList = new ArrayList<>();
+
+        // Add the mock calls to child classes
+        expect(mockCourtRepo.findAll()).andReturn(courtDaoList);
+
+        replay(mockCourtRepo);
+
+        // Perform the test
+        List<CourtDto> courtDtoList = classUnderTest.getCourts();
+
+        // Assert that the objects are as expected
+        assertTrue(courtDtoList.isEmpty(), "NOT Empty");
+
+        // Verify the expected mocks were called
+        verify(mockCourtRepo);
+
+    }
+
+    @Test
+    void emptyCourtSitesTest() {
+
+        List<XhbCourtSiteDao> courtSiteDaoList = new ArrayList<>();
+
+        // Add the mock calls to child classes
+        expect(mockCourtSiteRepo.findAll()).andReturn(courtSiteDaoList);
+
+        replay(mockCourtSiteRepo);
+
+        // Perform the test
+        List<XhibitCourtSiteDto> courtSiteDtoList = classUnderTest.getCourtSites(null);
+
+        // Assert that the objects are as expected
+        assertTrue(courtSiteDtoList.isEmpty(), "Not empty");
+
+        // Verify the expected mocks were called
+        verify(mockCourtSiteRepo);
+
+    }
+
+    @Test
+    void emptyCourtRoomsTest() {
+
+        List<XhbCourtRoomDao> courtRoomDaoList = new ArrayList<>();
+
+        // Add the mock calls to child classes
+        expect(mockCourtRoomRepo.findByCourtSiteId(1)).andReturn(courtRoomDaoList);
+
+        replay(mockCourtRoomRepo);
+
+        // Perform the test
+        List<CourtRoomDto> courtSiteDtoList = classUnderTest.getCourtRooms(1L);
+
+        // Assert that the objects are as expected
+        assertTrue(courtSiteDtoList.isEmpty(), "Not empty");
+
+        // Verify the expected mocks were called
+        verify(mockCourtRoomRepo);
+
+    }
+
+    @Test
+    void updateCourtRoomEmptyDaoTest() {
+
+        CourtRoomAmendCommand courtRoomAmendCommand = new CourtRoomAmendCommand();
+        courtRoomAmendCommand.setName("name");
+        courtRoomAmendCommand.setDescription(DESCRIPTION);
+        courtRoomAmendCommand.setCourtRoomId(1);
+
+        Optional<XhbCourtRoomDao> emptyDao = Optional.empty();
+
+        expect(mockCourtRoomRepo.findById(courtRoomAmendCommand.getCourtRoomId())).andReturn(emptyDao);
+
+        replay(mockCourtRoomRepo);
+
+        // Perform the test
+        classUnderTest.updateCourtRoom(courtRoomAmendCommand, new ArrayList<>());
+
+        // Verify the expected mocks were called
+        verify(mockCourtRoomRepo);
+
+    }
+}
--- src/test/java/uk/gov/hmcts/pdm/publicdisplay/manager/service/CourtRoomServiceTest.java
+++ src/test/java/uk/gov/hmcts/pdm/publicdisplay/manager/service/CourtRoomServiceTest.java
@@ -1,53 +1,32 @@
 package uk.gov.hmcts.pdm.publicdisplay.manager.service;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.easymock.Capture;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.pdm.business.entities.xhbcourt.XhbCourtDao;
-import uk.gov.hmcts.pdm.business.entities.xhbcourt.XhbCourtRepository;
-import uk.gov.hmcts.pdm.publicdisplay.common.test.AbstractJUnit;
+import uk.gov.hmcts.pdm.business.entities.xhbcourtroom.XhbCourtRoomDao;
+import uk.gov.hmcts.pdm.business.entities.xhbcourtsite.XhbCourtSiteDao;
 import uk.gov.hmcts.pdm.publicdisplay.manager.dto.CourtDto;
-import uk.gov.hmcts.pdm.publicdisplay.manager.service.api.ICourtRoomService;
+import uk.gov.hmcts.pdm.publicdisplay.manager.dto.CourtRoomDto;
+import uk.gov.hmcts.pdm.publicdisplay.manager.dto.XhibitCourtSiteDto;
+import uk.gov.hmcts.pdm.publicdisplay.manager.web.courtroom.CourtRoomAmendCommand;
+import uk.gov.hmcts.pdm.publicdisplay.manager.web.courtroom.CourtRoomCreateCommand;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.capture;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.newCapture;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
-class CourtRoomServiceTest extends AbstractJUnit {
-
-    /** The class under test. */
-    protected ICourtRoomService classUnderTest;
-
-    /** The mock Display Location Repo. */
-    protected XhbCourtRepository mockCourtRepo;
-
-
-    protected static final String NOT_EQUAL = "Not equal";
-    protected static final String FALSE = "False";
-
-
-    /**
-     * Setup.
-     */
-    @BeforeEach
-    public void setup() {
-        // Create a new version of the class under test
-        classUnderTest = new CourtRoomService();
-
-        // Setup the mock version of the called classes
-        mockCourtRepo = createMock(XhbCourtRepository.class);
-
-        ReflectionTestUtils.setField(classUnderTest, "xhbCourtRepository", mockCourtRepo);
-    }
+class CourtRoomServiceTest extends CourtRoomServiceEmptyTest {
 
     @Test
     void courtsTest() {
@@ -66,10 +45,10 @@ class CourtRoomServiceTest extends AbstractJUnit {
         replay(mockCourtRepo);
 
         // Perform the test
-        List<CourtDto> rotationSetsDtos = classUnderTest.getCourts();
+        List<CourtDto> courtDtoList = classUnderTest.getCourts();
 
         // Assert that the objects are as expected
-        assertEquals(courtDao.getCourtId(), rotationSetsDtos.get(0).getId(), NOT_EQUAL);
+        assertEquals(courtDao.getCourtId(), courtDtoList.get(0).getId(), NOT_EQUAL);
 
         // Verify the expected mocks were called
         verify(mockCourtRepo);
@@ -77,23 +56,146 @@ class CourtRoomServiceTest extends AbstractJUnit {
     }
 
     @Test
-    void emptyCourtsTest() {
+    void courtSitesTest() {
 
-        List<XhbCourtDao> courtDaoList = new ArrayList<>();
+        XhbCourtSiteDao xhbCourtSiteDao = new XhbCourtSiteDao();
+        xhbCourtSiteDao.setId(2);
+        xhbCourtSiteDao.setCourtSiteName("courtSiteName");
+        xhbCourtSiteDao.setCourtSiteCode("courtSiteCode");
+        xhbCourtSiteDao.setCourtId(4);
+
+        List<XhbCourtSiteDao> courtSiteDaoList = new ArrayList<>();
+        courtSiteDaoList.add(xhbCourtSiteDao);
 
         // Add the mock calls to child classes
-        expect(mockCourtRepo.findAll()).andReturn(courtDaoList);
+        expect(mockCourtSiteRepo.findByCourtId(1)).andReturn(courtSiteDaoList);
 
-        replay(mockCourtRepo);
+        replay(mockCourtSiteRepo);
 
         // Perform the test
-        List<CourtDto> courtDtoList = classUnderTest.getCourts();
+        List<XhibitCourtSiteDto> courtSiteDtoList = classUnderTest.getCourtSites(1);
 
         // Assert that the objects are as expected
-        assertTrue(courtDtoList.isEmpty(), "NOT Empty");
+        assertEquals(xhbCourtSiteDao.getId(), courtSiteDtoList.get(0).getId().intValue(), NOT_EQUAL);
 
         // Verify the expected mocks were called
-        verify(mockCourtRepo);
+        verify(mockCourtSiteRepo);
 
     }
+
+    @Test
+    void courtSitesCourtIdNullTest() {
+
+        XhbCourtSiteDao xhbCourtSiteDao = new XhbCourtSiteDao();
+        xhbCourtSiteDao.setId(1);
+        xhbCourtSiteDao.setCourtSiteName("courtSiteName");
+        xhbCourtSiteDao.setCourtSiteCode("courtSiteCode");
+        xhbCourtSiteDao.setCourtId(4);
+
+        List<XhbCourtSiteDao> courtSiteDaoList = new ArrayList<>();
+        courtSiteDaoList.add(xhbCourtSiteDao);
+
+        // Add the mock calls to child classes
+        expect(mockCourtSiteRepo.findAll()).andReturn(courtSiteDaoList);
+
+        replay(mockCourtSiteRepo);
+
+        // Perform the test
+        List<XhibitCourtSiteDto> courtSiteDtoList = classUnderTest.getCourtSites(null);
+
+        // Assert that the objects are as expected
+        assertEquals(xhbCourtSiteDao.getId(), courtSiteDtoList.get(0).getId().intValue(), NOT_EQUAL);
+
+        // Verify the expected mocks were called
+        verify(mockCourtSiteRepo);
+
+    }
+
+    @Test
+    void courtRoomsTest() {
+
+        XhbCourtRoomDao courtRoomDao = new XhbCourtRoomDao();
+        courtRoomDao.setCourtRoomId(1);
+        courtRoomDao.setCourtRoomName("courtRoomName");
+        courtRoomDao.setDescription(DESCRIPTION);
+        courtRoomDao.setCourtRoomNo(2);
+
+        List<XhbCourtRoomDao> courtRoomDaoList = new ArrayList<>();
+        courtRoomDaoList.add(courtRoomDao);
+
+        // Add the mock calls to child classes
+        expect(mockCourtRoomRepo.findByCourtSiteId(1)).andReturn(courtRoomDaoList);
+
+        replay(mockCourtRoomRepo);
+
+        // Perform the test
+        List<CourtRoomDto> courtSiteDtoList = classUnderTest.getCourtRooms(1L);
+
+        // Assert that the objects are as expected
+        assertEquals(courtRoomDao.getCourtRoomId(), courtSiteDtoList.get(0).getId().intValue(), NOT_EQUAL);
+
+        // Verify the expected mocks were called
+        verify(mockCourtRoomRepo);
+
+    }
+
+    @Test
+    void createDisplayTest() {
+
+        CourtRoomCreateCommand courtRoomCreateCommand = new CourtRoomCreateCommand();
+        courtRoomCreateCommand.setName("courtRoomCreateCommand");
+        courtRoomCreateCommand.setXhibitCourtSiteId(1L);
+        courtRoomCreateCommand.setDescription(DESCRIPTION);
+
+        // Capture the XhbDisplayDao
+        final Capture<XhbCourtRoomDao> courtRoomDaoCapture = newCapture();
+
+        // Add the mock calls to child classes
+        mockCourtRoomRepo.saveDao(capture(courtRoomDaoCapture));
+        expectLastCall();
+        replay(mockCourtRoomRepo);
+
+        // Perform the test
+        classUnderTest.createCourtRoom(courtRoomCreateCommand, new ArrayList<>());
+
+        // Assert that the objects are as expected
+        assertEquals(courtRoomCreateCommand.getName(),
+                courtRoomDaoCapture.getValue().getCourtRoomName(), NOT_EQUAL);
+        assertEquals(courtRoomCreateCommand.getDescription(),
+                courtRoomDaoCapture.getValue().getDescription(), NOT_EQUAL);
+        assertEquals(courtRoomCreateCommand.getDisplayName(),
+                courtRoomDaoCapture.getValue().getDisplayName(), NOT_EQUAL);
+        assertEquals(courtRoomCreateCommand.getXhibitCourtSiteId().intValue(),
+                courtRoomDaoCapture.getValue().getCourtSiteId(), NOT_EQUAL);
+
+        // Verify the expected mocks were called
+        verify(mockCourtRoomRepo);
+
+    }
+
+    @Test
+    void updateCourtRoomTest() {
+
+        CourtRoomAmendCommand courtRoomAmendCommand = new CourtRoomAmendCommand();
+        courtRoomAmendCommand.setName("name");
+        courtRoomAmendCommand.setDescription(DESCRIPTION);
+        courtRoomAmendCommand.setCourtRoomId(1);
+
+        XhbCourtRoomDao xhbCourtRoomDao = new XhbCourtRoomDao();
+        Optional<XhbCourtRoomDao> courtRoomDao = Optional.of(xhbCourtRoomDao);
+
+        expect(mockCourtRoomRepo.findById(courtRoomAmendCommand.getCourtRoomId())).andReturn(courtRoomDao);
+
+        // Add the mock calls to child classes
+        expect(mockCourtRoomRepo.updateDao(courtRoomDao.get())).andReturn(courtRoomDao);
+        replay(mockCourtRoomRepo);
+
+        // Perform the test
+        classUnderTest.updateCourtRoom(courtRoomAmendCommand, new ArrayList<>());
+
+        // Verify the expected mocks were called
+        verify(mockCourtRoomRepo);
+
+    }
+
 }
