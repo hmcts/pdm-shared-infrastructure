9d4e6445701cd24579255ef404757d20fe098ca5
markharriscgi <mark.harris1@hmcts.net>
2024-02-23 10:41:19 +0000

    pdda-347 Correct judge tests

--- src/test/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/judge/JudgeControllerBaseTest.java
+++ src/test/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/judge/JudgeControllerBaseTest.java
@@ -43,6 +43,10 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
     protected String viewNameCreateJudge;
     protected String viewNameAmendJudge;
     protected String viewNameDeleteJudge;
+    protected String mappingNameViewJudgeUrl;
+    protected String mappingNameCreateJudgeUrl;
+    protected String mappingNameAmendJudgeUrl;
+    protected String mappingNameDeleteJudgeUrl;
     protected MockMvc mockMvc;
     protected static final String NOT_NULL = "Not null";
     protected static final String NULL = "Null";
@@ -58,7 +62,7 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
     protected static final String JUDGE_TYPE_LIST = "judgeTypeList";
     protected static final String JUDGE_LIST = "judgeList";
     protected static final String COURTSITE = "courtSite";
-    protected static final String REQUEST_MAPPING = "REQUEST_MAPPING";
+    protected static final String REQUEST_MAPPING = "/judge";
     protected static final String ONE = "1";
     protected static final String REF_JUDGE_ID = "refJudgeId";
     protected static final String SURNAME = "surname";
@@ -82,29 +86,35 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
         // Setup the mock version of the called classes
         mockObjects();
         // Map the mock to the class under tests called class
-        ReflectionTestUtils.setField(classUnderTest, "judgeSelectedValidator", mockJudgeSelectedValidator);
-        ReflectionTestUtils.setField(classUnderTest, "judgeCreateValidator", mockJudgeCreateValidator);
-        ReflectionTestUtils.setField(classUnderTest, "judgeAmendValidator", mockJudgeAmendValidator);
-        ReflectionTestUtils.setField(classUnderTest, "judgeDeleteValidator", mockJudgeDeleteValidator);
-        ReflectionTestUtils.setField(classUnderTest, "judgePageStateHolder", mockJudgePageStateHolder);
+        ReflectionTestUtils.setField(classUnderTest, "judgeSelectedValidator",
+            mockJudgeSelectedValidator);
+        ReflectionTestUtils.setField(classUnderTest, "judgeCreateValidator",
+            mockJudgeCreateValidator);
+        ReflectionTestUtils.setField(classUnderTest, "judgeAmendValidator",
+            mockJudgeAmendValidator);
+        ReflectionTestUtils.setField(classUnderTest, "judgeDeleteValidator",
+            mockJudgeDeleteValidator);
+        ReflectionTestUtils.setField(classUnderTest, "judgePageStateHolder",
+            mockJudgePageStateHolder);
         ReflectionTestUtils.setField(classUnderTest, "refJudgeService", mockRefJudgeService);
 
         // Get the static variables from the class under test
         viewNameViewJudge =
-                ReflectionTestUtils.getField(classUnderTest, REQUEST_MAPPING)
-                        + (String) ReflectionTestUtils.getField(classUnderTest, "MAPPING_VIEW_JUDGE");
-
+            (String) ReflectionTestUtils.getField(classUnderTest, "VIEW_NAME_VIEW_JUDGE");
+        mappingNameViewJudgeUrl = REQUEST_MAPPING
+            + (String) ReflectionTestUtils.getField(classUnderTest, "MAPPING_VIEW_JUDGE");
         viewNameCreateJudge =
-                ReflectionTestUtils.getField(classUnderTest, REQUEST_MAPPING)
-                        + (String) ReflectionTestUtils.getField(classUnderTest, "MAPPING_CREATE_JUDGE");
-
+            (String) ReflectionTestUtils.getField(classUnderTest, "VIEW_NAME_CREATE_JUDGE");
+        mappingNameCreateJudgeUrl = REQUEST_MAPPING
+            + (String) ReflectionTestUtils.getField(classUnderTest, "MAPPING_CREATE_JUDGE");
         viewNameAmendJudge =
-                ReflectionTestUtils.getField(classUnderTest, REQUEST_MAPPING)
-                        + (String) ReflectionTestUtils.getField(classUnderTest, "MAPPING_AMEND_JUDGE");
-
+            (String) ReflectionTestUtils.getField(classUnderTest, "VIEW_NAME_AMEND_JUDGE");
+        mappingNameAmendJudgeUrl = REQUEST_MAPPING
+            + (String) ReflectionTestUtils.getField(classUnderTest, "MAPPING_AMEND_JUDGE");
         viewNameDeleteJudge =
-                ReflectionTestUtils.getField(classUnderTest, REQUEST_MAPPING)
-                        + (String) ReflectionTestUtils.getField(classUnderTest, "MAPPING_DELETE_JUDGE");
+            (String) ReflectionTestUtils.getField(classUnderTest, "VIEW_NAME_DELETE_JUDGE");
+        mappingNameDeleteJudgeUrl = REQUEST_MAPPING
+            + (String) ReflectionTestUtils.getField(classUnderTest, "MAPPING_DELETE_JUDGE");
 
         // Stop circular view path error
         final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -112,7 +122,8 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
         viewResolver.setSuffix(".jsp");
 
         // Setup the mock version of the modelMvc
-        mockMvc = MockMvcBuilders.standaloneSetup(classUnderTest).setViewResolvers(viewResolver).build();
+        mockMvc =
+            MockMvcBuilders.standaloneSetup(classUnderTest).setViewResolvers(viewResolver).build();
     }
 
     protected void mockObjects() {
@@ -156,13 +167,12 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
         replay(mockJudgePageStateHolder);
 
         // Perform the test
-        final MvcResult results =
-                mockMvc.perform(get(viewNameAmendJudge + "/9"))
-                       .andReturn();
+        final MvcResult results = mockMvc.perform(get(mappingNameAmendJudgeUrl + "/9")).andReturn();
         String response = results.getResponse().getContentAsString();
         RefJudgeDto returnedRefJudgeDto = new ObjectMapper().readValue(response, RefJudgeDto.class);
 
-        assertEquals(refJudgeDtos.get(0).getJudgeTypeDeCode(), returnedRefJudgeDto.getJudgeTypeDeCode(), NOT_EQUAL);
+        assertEquals(refJudgeDtos.get(0).getJudgeTypeDeCode(),
+            returnedRefJudgeDto.getJudgeTypeDeCode(), NOT_EQUAL);
         verify(mockJudgePageStateHolder);
     }
 
@@ -176,9 +186,7 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
         replay(mockJudgePageStateHolder);
 
         // Perform the test
-        final MvcResult results =
-                mockMvc.perform(get(viewNameAmendJudge + "/9"))
-                       .andReturn();
+        final MvcResult results = mockMvc.perform(get(mappingNameAmendJudgeUrl + "/9")).andReturn();
         String response = results.getResponse().getContentAsString();
         RefJudgeDto returnedRefJudgeDto = new ObjectMapper().readValue(response, RefJudgeDto.class);
 
@@ -201,13 +209,14 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
         replay(mockJudgePageStateHolder);
 
         // Perform the test
-        final MvcResult results =
-                mockMvc.perform(get(viewNameViewJudge).param(XHIBIT_COURTSITE_ID, "1")).andReturn();
+        final MvcResult results = mockMvc
+            .perform(get(mappingNameViewJudgeUrl).param(XHIBIT_COURTSITE_ID, "1")).andReturn();
         String returnedViewName = results.getModelAndView().getViewName();
 
         assertNotNull(results, NULL);
         assertEquals(viewNameViewJudge, returnedViewName, NOT_EQUAL);
-        assertEquals(xhibitCourtSiteDtos, results.getModelAndView().getModel().get(COURTSITE_LIST), NOT_EQUAL);
+        assertEquals(xhibitCourtSiteDtos, results.getModelAndView().getModel().get(COURTSITE_LIST),
+            NOT_EQUAL);
         assertEquals(xhibitCourtSiteDtos.get(0), capturedCourtSites.getValue().get(0), NOT_EQUAL);
 
         verify(mockRefJudgeService);
@@ -219,21 +228,21 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
         final List<XhibitCourtSiteDto> xhibitCourtSiteDtos = createCourtSiteDtoList();
         JudgeSearchCommand judgeSearchCommand = new JudgeSearchCommand();
 
-        expect(mockJudgePageStateHolder.getJudgeSearchCommand()).andReturn(judgeSearchCommand).times(2);
+        expect(mockJudgePageStateHolder.getJudgeSearchCommand()).andReturn(judgeSearchCommand)
+            .times(2);
         expect(mockJudgePageStateHolder.getSites()).andReturn(xhibitCourtSiteDtos);
         replay(mockJudgePageStateHolder);
 
         // Perform the test
-        final MvcResult results =
-                mockMvc.perform(get(viewNameViewJudge)
-                               .param(XHIBIT_COURTSITE_ID, "1")
-                               .param("reset", "false"))
-                       .andReturn();
+        final MvcResult results = mockMvc.perform(
+            get(mappingNameViewJudgeUrl).param(XHIBIT_COURTSITE_ID, "1").param("reset", "false"))
+            .andReturn();
         String returnedViewName = results.getModelAndView().getViewName();
 
         assertNotNull(results, NULL);
         assertEquals(viewNameViewJudge, returnedViewName, NOT_EQUAL);
-        assertEquals(xhibitCourtSiteDtos, results.getModelAndView().getModel().get(COURTSITE_LIST), NOT_EQUAL);
+        assertEquals(xhibitCourtSiteDtos, results.getModelAndView().getModel().get(COURTSITE_LIST),
+            NOT_EQUAL);
         verify(mockJudgePageStateHolder);
     }
 
@@ -253,16 +262,15 @@ abstract class JudgeControllerBaseTest extends AbstractJUnit {
         replay(mockJudgePageStateHolder);
 
         // Perform the test
-        final MvcResult results =
-                mockMvc.perform(get(viewNameViewJudge)
-                               .param(XHIBIT_COURTSITE_ID, "1")
-                               .param("reset", "false"))
-                       .andReturn();
+        final MvcResult results = mockMvc.perform(
+            get(mappingNameViewJudgeUrl).param(XHIBIT_COURTSITE_ID, "1").param("reset", "false"))
+            .andReturn();
         String returnedViewName = results.getModelAndView().getViewName();
 
         assertNotNull(results, NULL);
         assertEquals(viewNameViewJudge, returnedViewName, NOT_EQUAL);
-        assertEquals(xhibitCourtSiteDtos, results.getModelAndView().getModel().get(COURTSITE_LIST), NOT_EQUAL);
+        assertEquals(xhibitCourtSiteDtos, results.getModelAndView().getModel().get(COURTSITE_LIST),
+            NOT_EQUAL);
         assertEquals(xhibitCourtSiteDtos.get(0), capturedCourtSites.getValue().get(0), NOT_EQUAL);
 
         verify(mockRefJudgeService);
