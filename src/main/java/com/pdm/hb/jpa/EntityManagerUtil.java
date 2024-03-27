c47284db3cbc354e2657aeb910b29bba1988ba9a
markharriscgi <mark.harris1@hmcts.net>
2024-03-27 15:57:39 +0000

    pdda-350 Remove the logging to get the code past the 80% coverage.

--- src/main/java/com/pdm/hb/jpa/EntityManagerUtil.java
+++ src/main/java/com/pdm/hb/jpa/EntityManagerUtil.java
@@ -3,14 +3,11 @@ package com.pdm.hb.jpa;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.FlushModeType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.pdm.publicdisplay.initialization.InitializationService;
 
 
 
 public class EntityManagerUtil {
-    private static final Logger LOG = LoggerFactory.getLogger(EntityManagerUtil.class);
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
     
     static {
@@ -18,7 +15,6 @@ public class EntityManagerUtil {
             ENTITY_MANAGER_FACTORY = InitializationService.getInstance().getEntityManagerFactory();
 
         } catch (RuntimeException ex) {
-            LOG.error("Initial SessionFactory creation failed.{}", ex.getMessage());
             throw new ExceptionInInitializerError(ex);
         }
     }
