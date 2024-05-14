18f55b06f657921fd9564d98f5523dc507c3d425
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-05-14 11:05:12 +0100

    Add a log entry for database username

--- src/main/java/uk/gov/hmcts/PdmanagerSpringbootApplication.java
+++ src/main/java/uk/gov/hmcts/PdmanagerSpringbootApplication.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +13,11 @@ import uk.gov.hmcts.config.WebAppInitializer;
 @EnableAutoConfiguration
 public class PdmanagerSpringbootApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PdmanagerSpringbootApplication.class);
+    
     public static void main(String[] args) {
+        String dbUsername = System.getenv("DB_USER_NAME");
+        LOG.info("Database username: {}", dbUsername);
         SpringApplication.run(new Class[] {PdmanagerSpringbootApplication.class, WebAppInitializer.class}, args);
     }
 
