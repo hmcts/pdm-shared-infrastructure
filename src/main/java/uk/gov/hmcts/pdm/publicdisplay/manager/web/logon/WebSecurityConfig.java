bbdbf3d0764482d1593aad6140dc4e2b141321bb
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-09-23 10:05:50 +0100

    Correct the whitelist entry for login

--- src/main/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/logon/WebSecurityConfig.java
+++ src/main/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/logon/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.pdm.publicdisplay.manager.web.logon;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -7,13 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class WebSecurityConfig {
 
+    private static final Log LOG = LogFactory.getLog(WebSecurityConfig.class);
     private static final String[] AUTH_WHITELIST = {
         "/health/liveness",
         "/health/readiness",
         "/health",
         "/loggers/**",
         "/",
-        "/Login",
+        "/login",
         "/status/health",
         "/swagger-resources/**",
         "/swagger-ui/**",
@@ -22,6 +25,7 @@ public class WebSecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
+        LOG.info("webSecurityCustomizer()");
         return web -> web.ignoring().requestMatchers(AUTH_WHITELIST);
     }
 
