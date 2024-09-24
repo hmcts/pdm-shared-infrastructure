264cc86284ea3ed7bf2bdd0aa9d3323cff71c419
Mark Harris <156429052+markharriscgi@users.noreply.github.com>
2024-09-24 12:40:16 +0100

    Force the azure login to appear. (#230)

--- src/main/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/logon/WebSecurityConfig.java
+++ src/main/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/logon/WebSecurityConfig.java
@@ -13,13 +13,13 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 public class WebSecurityConfig {
 
     private static final Log LOG = LogFactory.getLog(WebSecurityConfig.class);
+    private static final String INVALIDSESSION_URL = "/invalidSession"; 
     private static final String[] AUTH_WHITELIST = {
         "/health/liveness",
         "/health/readiness",
         "/health",
         "/loggers/**",
         "/",
-        "/login**",
         "/error**",
         "/callback/",
         "/css/xhibit.css",
@@ -33,12 +33,18 @@ public class WebSecurityConfig {
         "/swagger-ui/**",
         "/webjars/**"
     };
-    
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         LOG.info("filterChain()");
         try {
-            http.csrf(csrf -> csrf.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()));
+            http.csrf(csrf -> csrf.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+                .sessionManagement(session -> session.invalidSessionUrl(INVALIDSESSION_URL))
+                .oauth2Login(oauth2Login -> oauth2Login
+                    .authorizationEndpoint(
+                        authorizationEndpoint -> authorizationEndpoint.baseUri("/oauth2/authorize"))
+                    .redirectionEndpoint(
+                        redirectionEndpoint -> redirectionEndpoint.baseUri("/oauth2/callback/*")));
             return http.build();
         } catch (Exception exception) {
             LOG.error("Failure in filterChain", exception);
