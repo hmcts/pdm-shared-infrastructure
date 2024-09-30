c20e866a26ec9ee1abe3e1f065cd6b76a8928cd1
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-09-30 11:25:27 +0100

    Add unit tests

--- src/main/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/authentication/RequestMatcher.java
+++ src/main/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/authentication/RequestMatcher.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 @FunctionalInterface
 public interface RequestMatcher {
 
-    String INTERNAL_URL_MATCHER = "/internal-user";
+    String INTERNAL_URL_MATCHER = "/login";
 
     RequestMatcher URL_MAPPER_INTERNAL = (req) -> req.getRequestURL().toString().contains(INTERNAL_URL_MATCHER);
 
--- src/test/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/authentication/InternalAuthConfigurationPropertiesStrategyTest.java
+++ src/test/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/authentication/InternalAuthConfigurationPropertiesStrategyTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.pdm.publicdisplay.manager.web.authentication;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,7 +10,10 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import uk.gov.hmcts.pdm.publicdisplay.common.test.AbstractJUnit;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 /**
  * The Class InternalAuthConfigurationPropertiesStrategy.
@@ -20,11 +24,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @MockitoSettings(strictness = Strictness.LENIENT)
 class InternalAuthConfigurationPropertiesStrategyTest extends AbstractJUnit {
 
+    private static final String FALSE = "Result is True";
     private static final String NOTNULL = "Result is Null";
+    private static final String TRUE = "Result is False";
 
 
     @InjectMocks
-    private final InternalAuthConfigurationPropertiesStrategy classUnderTest =
+    private final AuthenticationConfigurationPropertiesStrategy classUnderTest =
         new InternalAuthConfigurationPropertiesStrategy(
             Mockito.mock(InternalAuthConfigurationProperties.class),
             Mockito.mock(InternalAuthProviderConfigurationProperties.class));
@@ -39,5 +45,27 @@ class InternalAuthConfigurationPropertiesStrategyTest extends AbstractJUnit {
     void testGetProviderConfiguration() {
         AuthProviderConfigurationProperties result = classUnderTest.getProviderConfiguration();
         assertNotNull(result, NOTNULL);
+    }
+    
+    @Test
+    void testDoesMatch() {
+        StringBuffer validSb = new StringBuffer();
+        validSb.append("/login");
+        HttpServletRequest mockHttpServletRequest = Mockito.mock(HttpServletRequest.class);
+        Mockito.when(mockHttpServletRequest.getRequestURL()).thenReturn(validSb);
+        
+        boolean result = classUnderTest.doesMatch(mockHttpServletRequest);
+        assertTrue(result, TRUE);
+    }
+    
+    @Test
+    void testDoesMatchFailure() {
+        StringBuffer validSb = new StringBuffer();
+        validSb.append("/invalid");
+        HttpServletRequest mockHttpServletRequest = Mockito.mock(HttpServletRequest.class);
+        Mockito.when(mockHttpServletRequest.getRequestURL()).thenReturn(validSb);
+        
+        boolean result = classUnderTest.doesMatch(mockHttpServletRequest);
+        assertFalse(result, FALSE);
     }
 }
