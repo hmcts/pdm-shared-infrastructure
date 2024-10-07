ceecec6b115e04a96d9e7a93892a11161c0bc895
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-10-07 13:00:29 +0100

    Add the authentication model

---
+++ src/main/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/authentication/model/SecurityToken.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.pdm.publicdisplay.manager.web.authentication.model;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Builder
+@Value
+public class SecurityToken {
+
+    private String accessToken;
+    private UserState userState;
+
+}
---
+++ src/main/java/uk/gov/hmcts/pdm/publicdisplay/manager/web/authentication/model/UserStateRole.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.pdm.publicdisplay.manager.web.authentication.model;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode.Exclude;
+import lombok.EqualsAndHashCode.Include;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.Set;
+
+@Builder
+@Value
+@Jacksonized
+public class UserStateRole {
+
+    @Include
+    @NonNull
+    private Integer roleId;
+    @Include
+    @NonNull
+    private String roleName;
+    @NonNull
+    private Boolean globalAccess;
+    @Exclude
+    private Set<Integer> courthouseIds;
+    @Exclude
+    @NonNull
+    private Set<String> permissions;
+
+}
